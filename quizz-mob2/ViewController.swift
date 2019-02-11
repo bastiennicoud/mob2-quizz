a3451c89c317ab82a757b7ce5b584b0e697bcb8b
bastien <bastien.nicoud@gmail.com>
2019-02-11 18:07:24 +0100

    Initial Commit

---
+++ quizz-mob2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  quizz-mob2
+//
+//  Created by Bastien Nicoud on 11.02.19.
+//  Copyright © 2019 CPNV-ES. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
