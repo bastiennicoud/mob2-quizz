2a34aa2805d4f205075dc5a5f78e11cc1e407826
bastien <bastien.nicoud@gmail.com>
2019-02-11 20:35:38 +0100

    Add outlets to main controller

--- quizz-mob2/ViewController.swift
+++ quizz-mob2/ViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    /**
+     Outlets
+     */
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var hintButton: UIButton!
+    @IBOutlet weak var answerButton1: UIButton!
+    @IBOutlet weak var answerButton2: UIButton!
+    @IBOutlet weak var answerButton3: UIButton!
+    @IBOutlet weak var quizzProgressBar: UIProgressView!
+    
+    /**
+     Display questions when view loaded
+     */
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    /**
+     Action called when you touch the information button.
+     */
+    @IBAction func onHintButtonTouch() {
+    }
+    
+    /**
+     Action called when you pinch the screen with two fingers.
+     */
+    @IBAction func onScreenPinch(_ sender: UIPinchGestureRecognizer) {
+    }
+    
+    /**
+     Action called when you touch an answer.
+     */
+    @IBAction func onAnswerTouch(_ sender: UIButton) {
+    }
+    
 }
 
